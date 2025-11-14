e43afa07e3969ade59243926276f16c88bd5f79b
MalenaBagnera <malenabagnera08@gmail.com>
2025-11-14 16:02:23 -0300

    Refactor Usuario class constructor and methods

--- Usuario.js
+++ Usuario.js
@@ -1,41 +1,37 @@
 class Usuario {
-
-  constructor( e, img) {
+  constructor(enemigos, img, intentos) {
     this.x = 0;
     this.y = 350;
     this.t = 140;
-    this.v = 10;
-    this.c = 255;
-
-    this.enemigo = e;
-    this.img= img;
+    this.v = 17;  
+    this.enemigo = enemigos;
+    this.img = img;
+    this.intentos = intentos;
   }
 
   dibujar() {
-
-    image (this.img, this.x, this.y, this.t, this.t);
+    image(this.img, this.x, this.y, this.t, this.t);
   }
 
-  mover() {
-    if (keyCode === LEFT_ARROW ) {
+  mover(juego) {
+     if (keyIsDown(LEFT_ARROW)) this.x -= this.v;
+  if (keyIsDown(RIGHT_ARROW)) this.x += this.v;
 
-      this.x -= this.v;
-    }
-    if (keyCode === RIGHT_ARROW ) {
-
-      this.x += this.v;
-    }
-    if (this.x + this.t >= width && ganaste === false  ) {
-      ganaste = true ;
-    }
+  if (this.x + this.t >= width && !juego.ganaste) {
+    juego.ganaste = true;
+    juego.pantalla = "ganaste"; 
+  }
   }
 
   colisionar(juego) {
-    for (let i = 0; i < 10; i++) {
-      if (dist(this.x, this.y, this.enemigo[i].x, this.enemigo[i].y) < this.t /2) {
-        intentos.sumar ();
-        juego.reiniciar(); // reinicia todo el juego
-        return; // corta el bucle
+    for (let i = 0; i < this.enemigo.length; i++) {
+      let p = this.enemigo[i];
+      if (dist(this.x, this.y, p.x, p.y) < this.t / 2) {
+        this.intentos.sumar(juego); // suma intento y revisa si pierde
+        if (this.intentos.cantidad < this.intentos.maximo && juego.pantalla === "juego") {
+          juego.reiniciar();
+        }
+        return; 
       }
     }
   }
