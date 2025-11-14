bec1034d8828ded38b020970ce8c963d37dd5e26
MalenaBagnera <malenabagnera08@gmail.com>
2025-11-14 16:01:35 -0300

    Refactor Particula class for cleaner code

--- Particula.js
+++ Particula.js
@@ -1,35 +1,24 @@
 class Particula {
-
-
-  constructor( velocidad, med) {  //parametros de constructor
-
-    this.x = Math.floor( Math.random() * 800 );
+  constructor(velocidad, med) {
+    this.x = Math.floor(Math.random() * 800);
     this.y = -100;
     this.diam = 40;
     this.vel = velocidad;
     this.mostrar = true;
-
     this.med = med;
   }
 
-
   dibujar() {
-    if ( this.mostrar ) {
-
-      image( this.med, this.x, this.y, this.diam, this.diam );
-    }
+    if (this.mostrar) image(this.med, this.x, this.y, this.diam, this.diam);
   }
 
   actualizar() {
-    if ( this.mostrar ) {
-
-      this.y = this.y + this.vel;  //suma la velocidad a la pos Y
-    }
+    if (this.mostrar) this.y += this.vel;
   }
 
   reciclar() {
-    if ( this.y >= height+100 ) {
-      this.x = int( random(width) );
+    if (this.y >= height + 100) {
+      this.x = int(random(width));
       this.y = -100;
       this.diam = 40;
     }
